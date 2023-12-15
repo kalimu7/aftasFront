2f6a7cbff8913e26ec01afbc3d9dd4865c6a2e44
kalimu7 <mahjourkarim@gmail.com>
2023-12-15 09:17:38 +0100

    first commmit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompetitionComponent } from './Component/competition/competition.component';
+import { MemberComponent } from './Component/member/member.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"competition",component:CompetitionComponent},
+  {path:"member",component:MemberComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
